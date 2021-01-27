db90ff1b8f25338e60f2d72892a673adc3bc0c1b
Luke <uklukehemmings@gmail.com>
2021-01-27 17:54:40 +0000

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Luke on 27/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
